f99bbe63118a49888c3fee96b012699dc51fe652
jiaxinliu <jiaxinliu.victor@gmail.com>
2025-01-20 08:22:32 -0500

    Add tests for int literals

---
+++ test_proj_1/sample_test_literal_int_invalid_1.c
@@ -0,0 +1,5 @@
+int f() {
+    int d;
+    /* Java method Integer.decode cannot parse a value exceeding the maximum range of Java int */
+    d= 1111111111111111111111111111111;
+}
---
+++ test_proj_1/sample_test_literal_int_invalid_2.c
@@ -0,0 +1,4 @@
+int f() {
+    int d;
+    d= 123456abcde;
+}
